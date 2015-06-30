e2bff4e9ec8aec326f1b2ded15d8f9503c6d54b9
Ceras <fredrik_orback@hotmail.com>
2015-06-30 12:17:03 +0200

    removed unused file

--- frontend/app/workdayViewer/activitiesInWorkdaySelection.jsx
+++ frontend/app/workdayViewer/activitiesInWorkdaySelection.jsx
@@ -3,12 +3,22 @@ var React = require('react');
 module.exports = React.createClass({
     render: function() {
         var renderedActivities = this.props.activities.map(function (activity) {
+            //var activityWorkdays = _.filter(this.props.workdays, function(workday){
+            //    return workday.activity.id === activity.id;
+            //});
+            //var activityHours = activityWorkdays.map(function(workday){
+            //    return workday.hours
+            //}).reduce(function(total, currentValue){
+            //    return total + currentValue
+            //});
+            //activityHours = Math.round(activityHours*100)/100;
+
             return (
                 <li>
                     {activity.name} | <i>{activity.offerArea.name}</i>
                 </li>
             )
-        });
+        }, this);
 
         return (
             <div id="activitiesInSelection">
