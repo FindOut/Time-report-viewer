a994006d698436542841653d52d000607b17270b
Ceras <fredrik_orback@hotmail.com>
2015-10-14 14:43:18 +0200

    created a reload data action

--- backend/grails-app/controllers/backend/PageController.groovy
+++ backend/grails-app/controllers/backend/PageController.groovy
@@ -1,19 +1,30 @@
 package backend
 
 import grails.plugin.springsecurity.annotation.Secured
+import groovy.sql.Sql
 
 class PageController {
+    def dataSource
+    def importDataService
+
     @Secured(['IS_AUTHENTICATED_FULLY'])
     def index() {
         render view: '../index'
     }
 
-    def clearDB(){
-        ActivityReport.executeUpdate('delete from ActivityReport')
-        Employee.executeUpdate('delete from Employee')
-        Activity.executeUpdate('delete from Activity')
+    @Secured(['IS_AUTHENTICATED_FULLY'])
+    def reloadDB(){
+        Sql sql = new Sql(dataSource.getConnection())
+        List applicationDomainObjects = grailsApplication.domainClasses
 
-        render status: 200
+        sql.execute("SET FOREIGN_KEY_CHECKS=0")
 
+        applicationDomainObjects.each{ object ->
+            object.clazz.executeUpdate("delete from ${object.getName()}")
+        }
+
+        sql.execute("SET FOREIGN_KEY_CHECKS=1")
+
+        importDataService.importDataFromDropbox()
     }
 }
